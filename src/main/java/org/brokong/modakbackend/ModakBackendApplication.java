4b630483d94000f5f83ae66fdc045410a22e0513
changbro <chxngbro@gmail.com>
2025-05-26 22:26:49 +0900

    [add] 초기 파일 추가

---
+++ src/main/java/org/brokong/modakbackend/ModakBackendApplication.java
@@ -0,0 +1,13 @@
+package org.brokong.modakbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModakBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModakBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/brokong/modakbackend/ModakBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.brokong.modakbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModakBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
