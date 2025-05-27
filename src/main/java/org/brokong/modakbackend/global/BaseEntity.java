2e52284797219fbb04b5c8a0a357ce48976ab279
changhyeong.lee <changhyeong.lee@lemonhc.com>
2025-05-27 09:45:44 +0900

    [add] 모든 엔티티의 Base가 되는 BaseEntity 생성 (id, 생성일, 수정일)

---
+++ src/main/java/org/brokong/modakbackend/global/BaseEntity.java
@@ -0,0 +1,25 @@
+package org.brokong.modakbackend.global;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@CreatedDate
+	@Column(nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(nullable = false)
+	private LocalDateTime modifiedAt;
+}
