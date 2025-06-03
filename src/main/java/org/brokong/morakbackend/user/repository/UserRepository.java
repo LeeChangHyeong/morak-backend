f0a94c67814a1c5f0ac6c582d9e73fe6cea93dff
changbro <chxngbro@gmail.com>
2025-06-03 17:24:38 +0900

    [feat] UserRepository에 닉네임 및 이메일 중복 확인 메서드 추가

--- src/main/java/org/brokong/morakbackend/user/repository/UserRepository.java
+++ src/main/java/org/brokong/morakbackend/user/repository/UserRepository.java
@@ -1,5 +1,20 @@
 package org.brokong.morakbackend.user.repository;
 
-public class UserRepository {
+import org.brokong.morakbackend.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // 이메일로 유저찾기
+    Optional<User> findByEmail(String email);
+
+    // 닉네임 중복 확인
+    boolean existsByNickname(String nickname);
+
+    // 이메일 중복 확인
+    boolean existsByEmail(String email);
 }
