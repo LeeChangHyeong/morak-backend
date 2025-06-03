b7152f93fea1848e6776b879c5ac5c9c4232cea1
changbro <chxngbro@gmail.com>
2025-06-03 16:02:38 +0900

    [feat] WebSecurityConfig에 인증 예외 경로 및 CSRF 비활성화 설정 추가

---
+++ src/main/java/org/brokong/morakbackend/global/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package org.brokong.morakbackend.global;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    @Value("${api.prefix}")
+    private String apiPrefix;
+
+    // 특정 경로 Security Off
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/static/**");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable()) // JWT 방식에서는 CSRF 비활성화
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                apiPrefix + "/auth/signup",
+                                apiPrefix + "/auth/login",
+                                apiPrefix + "/auth/login/**",
+                                apiPrefix + "/auth/logout"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form.disable())
+                .httpBasic(httpBasic -> httpBasic.disable());
+
+        return http.build();
+    }
+}
+
