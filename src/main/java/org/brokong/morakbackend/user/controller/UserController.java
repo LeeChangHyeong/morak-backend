fd0c70b7a61e27f9249ce14aaec9fc0fc5d38878
changbro <chxngbro@gmail.com>
2025-06-03 14:58:57 +0900

    [add] UserController 유저 신고, 유저 탈퇴, 유저 차단 api 추가

--- src/main/java/org/brokong/morakbackend/user/controller/UserController.java
+++ src/main/java/org/brokong/morakbackend/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package org.brokong.morakbackend.user.controller;
 
 import java.util.List;
+
 import org.brokong.morakbackend.global.ResponseDto;
 import org.brokong.morakbackend.user.dto.response.UserResponseDto;
 import org.springframework.http.HttpStatus;
@@ -11,26 +12,51 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/users")
 public class UserController {
 
-	// 단일 유저 조회
-	@GetMapping("/{userId}")
-	public ResponseEntity<ResponseDto<UserResponseDto>> getUser(@PathVariable Long userId) {
+    // 단일 유저 조회
+    @GetMapping("/{userId}")
+    public ResponseEntity<ResponseDto<UserResponseDto>> getUser(@PathVariable Long userId) {
 
-		ResponseDto<UserResponseDto> response = new ResponseDto<>("단일 사용자 조회 성공", new UserResponseDto());
-		return ResponseEntity.status(HttpStatus.OK).body(response);
-	}
+        ResponseDto<UserResponseDto> response = new ResponseDto<>("단일 사용자 조회 성공", new UserResponseDto());
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 
-	// 다중 유저 조회
-	@GetMapping("/batch")
-	public ResponseEntity<ResponseDto<List<UserResponseDto>>> getUsersByIds(@RequestParam List<Long> userIds) {
+    // 다중 유저 조회
+    @GetMapping("/batch")
+    public ResponseEntity<ResponseDto<List<UserResponseDto>>> getUsersByIds(@RequestParam List<Long> userIds) {
 
-		ResponseDto<List<UserResponseDto>> response = new ResponseDto<>("다중 사용자 조회 성공", List.of(new UserResponseDto()));
-		return ResponseEntity.status(HttpStatus.OK).body(response);
-	}
+        ResponseDto<List<UserResponseDto>> response = new ResponseDto<>("다중 사용자 조회 성공", List.of(new UserResponseDto()));
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 
-	// 전체 유저 조회
-	@GetMapping
-	public ResponseEntity<ResponseDto<List<UserResponseDto>>> getAllUsers() {
-		ResponseDto<List<UserResponseDto>> response = new ResponseDto<>("전체 사용자 조회 성공", List.of(new UserResponseDto()));
-		return ResponseEntity.status(HttpStatus.OK).body(response);
-	}
+    // 전체 유저 조회
+    @GetMapping
+    public ResponseEntity<ResponseDto<List<UserResponseDto>>> getAllUsers() {
+        ResponseDto<List<UserResponseDto>> response = new ResponseDto<>("전체 사용자 조회 성공", List.of(new UserResponseDto()));
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    // 유저 신고
+    @PostMapping("/{userId}/report")
+    public ResponseEntity<ResponseDto<String>> reportUser(@PathVariable Long userId) {
+        ResponseDto<String> response = new ResponseDto<>("사용자 신고 성공", "User reported successfully");
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    // 유저 탈퇴
+    @PostMapping("/withdrawal")
+    public ResponseEntity<ResponseDto<String>> withdrawal() {
+        ResponseDto<String> response = new ResponseDto<>("회원탈퇴 성공", "회원탈퇴 되었습니다.");
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    // TODO: 관리자 계정만
+    // 유저 차단
+    @PostMapping("{userId}/block")
+    public ResponseEntity<ResponseDto<String>> blockUser(@PathVariable Long userId) {
+        ResponseDto<String> response = new ResponseDto<>("유저 차단 성공", "유저가 차단되었습니다.");
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 }
