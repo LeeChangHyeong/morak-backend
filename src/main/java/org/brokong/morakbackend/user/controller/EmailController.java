c10aa76cb244d35365e165390112e700016e27f9
changhyeong.lee <changhyeong.lee@lemonhc.com>
2025-06-04 17:13:44 +0900

    [feat] 이메일 인증 코드 전송 및 검증 로직 구현

--- src/main/java/org/brokong/morakbackend/user/controller/EmailController.java
+++ src/main/java/org/brokong/morakbackend/user/controller/EmailController.java
@@ -1,8 +1,10 @@
 package org.brokong.morakbackend.user.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.brokong.morakbackend.global.ResponseDto;
 import org.brokong.morakbackend.user.dto.request.EmailVerifyReqeustDto;
 import org.brokong.morakbackend.user.dto.response.UserResponseDto;
+import org.brokong.morakbackend.user.service.EmailService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +15,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("${api.prefix}/email")
 public class EmailController {
+
+    private final EmailService emailService;
+
 
     // 이메일 중복 확인
     @GetMapping("check-email")
@@ -26,10 +32,11 @@ public class EmailController {
 
     // 이메일 인증번호 전송
     @PostMapping("send-email")
-    public ResponseEntity<ResponseDto> sendEmail(@RequestParam String email) {
-        ResponseDto<UserResponseDto> response = new ResponseDto<>("이메일 인증번호 전송 성공", new UserResponseDto());
+    public ResponseEntity<ResponseDto<Void>> sendEmail(@RequestParam String email) {
+        emailService.sendAuthCode(email);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(new ResponseDto<>("이메일 인증번호 전송 성공", null));
     }
 
     // 이메일 인증번호 확인
--- src/main/java/org/brokong/morakbackend/user/service/EmailService.java
+++ src/main/java/org/brokong/morakbackend/user/service/EmailService.java
@@ -1,10 +1,16 @@
 package org.brokong.morakbackend.user.service;
 
+import java.time.Duration;
+import java.util.Random;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.brokong.morakbackend.global.service.RedisService;
 import org.brokong.morakbackend.user.repository.UserRepository;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class EmailService {
@@ -12,7 +18,7 @@ public class EmailService {
     private final UserRepository userRepository;
     private final RedisService redisService;
     // TODO: 이메일 전송 기능 구현 해야함
-//    private final EmailSender emailSender;
+    private final JavaMailSender mailSender;
 
     // 이메일 중복 확인
     public void checkEmail(String email) {
@@ -22,17 +28,33 @@ public class EmailService {
     }
 
     // 이메일 검증 코드 전송
-    public void sendVerificationCode(String email) {
+    public void sendAuthCode(String email) {
+        String authCode = createAuthCode();
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("[모락(morak)] 이메일 인증 코드입니다.");
+        message.setText("인증번호: " + authCode + "\n인증번호를 입력창에 입력해주세요.");
 
+        try {
+            mailSender.send(message);
+        } catch (Exception e) {
+            log.error("[이메일 전송 실패] email: {}, message: {}", email, e.getMessage(), e);
+
+            throw new IllegalStateException("이메일 전송에 실패했습니다. 나중에 다시 시도해주세요.");
+        }
+
+        // 성공 시 Redis 저장
+        redisService.setValues("email_auth:" + email, authCode, Duration.ofMinutes(3));
     }
 
     // 이메일 코드 검증
-    public void verifyCode(String email, String code) {
+    public void verifyCode(String email, String authCode) {
 
     }
 
     // 이메일 검증 코드 생성
-    private String generateVerificationCode() {
-        return String.valueOf((int) (Math.random() * 900000) + 100000); // 6자리 숫자
+    public String createAuthCode() {
+        Random random = new Random();
+        return String.format("%06d", random.nextInt(1000000)); // 6자리 숫자
     }
 }
