195567abf0caf05c47b1ce2c49d199344df25915
changhyeong.lee <changhyeong.lee@lemonhc.com>
2025-06-04 17:23:55 +0900

    [refactor] RedisService 메서드 이름 변경: setValues, getData, deleteData -> setValue, getValue, deleteValue

--- src/main/java/org/brokong/morakbackend/global/service/RedisService.java
+++ src/main/java/org/brokong/morakbackend/global/service/RedisService.java
@@ -13,17 +13,17 @@ public class RedisService {
     private final RedisTemplate<String, String> redisTemplate;
 
     // 저장
-    public void setValues(String key, String value, Duration duration) {
+    public void setValue(String key, String value, Duration duration) {
         redisTemplate.opsForValue().set(key, value, duration);
     }
 
     // 조회
-    public String getData(String key) {
+    public String getValue(String key) {
         return redisTemplate.opsForValue().get(key);
     }
 
     // 삭제
-    public void deleteData(String key) {
+    public void deleteValue(String key) {
         redisTemplate.delete(key);
     }
 
