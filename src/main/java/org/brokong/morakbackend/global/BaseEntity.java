3de2663c3663d681ce88902d7981dd70fe0951ae
changbro <chxngbro@gmail.com>
2025-06-06 16:42:59 +0900

    [feat] BaseEntity에 EntityListeners 및 MorakBackendApplication에 EnableJpaAuditing 추가

--- src/main/java/org/brokong/morakbackend/global/BaseEntity.java
+++ src/main/java/org/brokong/morakbackend/global/BaseEntity.java
@@ -2,12 +2,15 @@ package org.brokong.morakbackend.global;
 
 import jakarta.persistence.Column;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class) // ⬅ 추가!
 public abstract class BaseEntity {
 
 	@CreatedDate
