8684a1825d15a875072b70f93f28d3ad054564f7
changbro <chxngbro@gmail.com>
2025-06-06 16:43:57 +0900

    [feat] UserResponseDto에 User 엔티티를 기반으로 한 생성자 추가: 이메일 및 닉네임 초기화

--- src/main/java/org/brokong/morakbackend/user/dto/response/UserResponseDto.java
+++ src/main/java/org/brokong/morakbackend/user/dto/response/UserResponseDto.java
@@ -1,10 +1,16 @@
 package org.brokong.morakbackend.user.dto.response;
 
 import lombok.Getter;
+import org.brokong.morakbackend.user.entity.User;
 
 @Getter
 public class UserResponseDto {
 
 	private String email;
 	private String nickname;
+
+	public UserResponseDto(User user) {
+		this.email = user.getEmail();
+		this.nickname = user.getNickname();
+	}
 }
