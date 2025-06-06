a29e8a9e129d18b077568f6cbdc0606e6f36f2d0
changbro <chxngbro@gmail.com>
2025-06-06 22:16:32 +0900

    [refactor] BaseEntity 패키지 경로 변경 및 코드 정리

--- src/main/java/org/brokong/morakbackend/global/entity/BaseEntity.java
+++ src/main/java/org/brokong/morakbackend/global/entity/BaseEntity.java
@@ -1,10 +1,12 @@
-package org.brokong.morakbackend.global;
+package org.brokong.morakbackend.global.entity;
 
 import jakarta.persistence.Column;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+
 import java.time.LocalDateTime;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -13,11 +15,11 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(AuditingEntityListener.class) // ⬅ 추가!
 public abstract class BaseEntity {
 
-	@CreatedDate
-	@Column(nullable = false, updatable = false)
-	private LocalDateTime createdAt;
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 
-	@LastModifiedDate
-	@Column(nullable = false)
-	private LocalDateTime modifiedAt;
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt;
 }
