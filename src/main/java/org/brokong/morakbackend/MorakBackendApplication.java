fcf91a5d066871b032dce2b3b446cfce8485780b
changhyeong.lee <changhyeong.lee@lemonhc.com>
2025-06-10 11:50:22 +0900

    [feat] 메서드 보안 활성화: Spring Security의 메서드 보안 기능을 활성화하는 어노테이션 추가

--- src/main/java/org/brokong/morakbackend/MorakBackendApplication.java
+++ src/main/java/org/brokong/morakbackend/MorakBackendApplication.java
@@ -3,9 +3,11 @@ package org.brokong.morakbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableMethodSecurity
 public class MorakBackendApplication {
 
 	public static void main(String[] args) {
