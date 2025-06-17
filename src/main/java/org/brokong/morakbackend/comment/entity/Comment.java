a9e5ba45125247fa5a6cf98e04a942c904982b12
LeeChangHyeong <chxngbro@gmail.com>
2025-06-17 16:25:15 +0900

    [feat] 댓글 테이블에 인덱스 추가: Comment 엔티티에 parent_comment_id에 대한 인덱스 추가

--- src/main/java/org/brokong/morakbackend/comment/entity/Comment.java
+++ src/main/java/org/brokong/morakbackend/comment/entity/Comment.java
@@ -6,9 +6,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Builder;
@@ -22,6 +24,9 @@ import org.brokong.morakbackend.user.entity.User;
 @Getter
 @RequiredArgsConstructor
 @Entity(name = "comments")
+@Table(indexes = {
+    @Index(name = "idx_parent_comment_id", columnList = "parent_comment_id")
+})
 public class Comment extends BaseEntity {
 
     @Id
