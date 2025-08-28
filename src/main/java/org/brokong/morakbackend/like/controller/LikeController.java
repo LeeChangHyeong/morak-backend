c79a59f0ec202b06ec360598ee469fe68158398d
LeeChangHyeong <chxngbro@gmail.com>
2025-08-28 13:36:31 +0900

    [delete] LikeController.java LikeService.java 삭제

--- src/main/java/org/brokong/morakbackend/like/controller/LikeController.java
+++
@@ -1,5 +0,0 @@
-package org.brokong.morakbackend.like.controller;
-
-public class LikeController {
-
-}
--- src/main/java/org/brokong/morakbackend/like/service/LikeService.java
+++
@@ -1,5 +0,0 @@
-package org.brokong.morakbackend.like.service;
-
-public class LikeService {
-
-}
