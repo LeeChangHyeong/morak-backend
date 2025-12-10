cc56762e565904c2a8a5facb733b8401e830416e
LeeChangHyeong <chxngbro@gmail.com>
2025-12-10 16:16:37 +0900

    [feat] Post 엔티티에 modifiedAt 필드 추가 및 관리 로직 구현: 게시글 수정 시 타임스탬프 업데이트 (수동 관리)

--- src/main/java/org/brokong/morakbackend/post/entity/Post.java
+++ src/main/java/org/brokong/morakbackend/post/entity/Post.java
@@ -1,6 +1,7 @@
 package org.brokong.morakbackend.post.entity;
 
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,10 +11,13 @@ import org.brokong.morakbackend.user.entity.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @NoArgsConstructor
 @Entity
+// modifiedAt은 Post에서 수동 관리
+@EntityListeners({})
 @Table(indexes = {
     // 1. 전체 게시글 - 시간순 정렬 (가장 기본적인 쿼리)
     @Index(name = "idx_post_created_at", columnList = "created_at"),
@@ -59,11 +63,16 @@ public class Post extends BaseEntity {
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<PostLike> likes = new ArrayList<>();
 
+    // modifiedAt을 Post에서 직접 관리
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt;
+
 
     @Builder
     public Post(User user, String content) {
         this.user = user;
         this.content = content;
+        this.modifiedAt = LocalDateTime.now();
     }
 
     public void decreaseLikeCount() {
@@ -84,6 +93,7 @@ public class Post extends BaseEntity {
 
     public void updateContent(String content) {
         this.content = content;
+        this.modifiedAt = LocalDateTime.now();  // 실제 내용 수정 시에만 업데이트
     }
 
     public void increaseViewCount() {
